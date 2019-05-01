54af19949fb2b9be9c7c8bcb54266572ab48f874
nitrotube <eg.spirin@gmail.com>
2019-05-01 21:22:27 +0200

    Added dependencies installationfile

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install lm-sensors
+sudo apt-get install lscpu
+sudo apt-get install htop
+sudo apt-get install stress-ng
+sudo apt-get install lm-sensors
+sudo apt-get install glmark2
+sudo apt-get install indicator-cpufreq
+sudo apt-get install intel-gpu-tools
