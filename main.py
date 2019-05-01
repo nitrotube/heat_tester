a5cc34f2c93054bd83ed1a10e72b4c58c1fa271f
nitrotube <eg.spirin@gmail.com>
2019-05-01 21:08:57 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import subprocess
+import datetime
+import time
+import csv
+
+STAT_INTERVAL = 1
+
+date_string = str(datetime.datetime.now().strftime('%d_%m_%y_%H_%M_%S'))
+print(date_string)
+
+with open(date_string+'.csv', 'w', newline='') as csvfile:
+    my_writer = csv.writer(csvfile, delimiter=',',
+                           quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    my_writer.writerow(['Zeit', 'Core 0 Temp', 'Core 1 Temp', 'CPU Freq'])
+    while True:
+        time_string = str(datetime.datetime.now().strftime("%X"))
+        print(time_string)
+
+        core0_temp = subprocess.check_output("sensors | grep 'Core 0'", shell=True, universal_newlines=True)
+        core0_temp = core0_temp[core0_temp.find('+')+1:core0_temp.find('+')+4]
+
+        if core0_temp[2] == '.':
+            core0_temp = core0_temp[:2]
+        core0_temp = int(core0_temp)
+        print(core0_temp)
+
+        core1_temp = subprocess.check_output("sensors | grep 'Core 1'", shell=True, universal_newlines=True)
+        core1_temp = core1_temp[core1_temp.find('+') + 1:core1_temp.find('+') + 4]
+
+        if core1_temp[2] == '.':
+            core1_temp = core1_temp[:2]
+        core1_temp = int(core1_temp)
+        print(core1_temp)
+
+        cpu_frequency = subprocess.check_output("lscpu | grep 'CPU MHz'", shell=True, universal_newlines=True)
+        cpu_frequency = int(cpu_frequency[cpu_frequency.find('.')-4:cpu_frequency.find('.')])
+        print(cpu_frequency)
+
+        my_writer.writerow([time_string, core0_temp, core1_temp, cpu_frequency])
+
+        time.sleep(STAT_INTERVAL)
