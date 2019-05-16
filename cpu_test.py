f40b86bb3d433783b2395bcc48345b5581477484
nitrotube <eg.spirin@gmail.com>
2019-05-16 10:44:23 +0200

    New scripts

--- cpu_test.py
+++ cpu_test.py
@@ -2,25 +2,29 @@ import subprocess
 import datetime
 import time
 import csv
+import os
 
 STAT_INTERVAL = 1
+STAT_TIME_LIMIT = 600
 
 date_string = str(datetime.datetime.now().strftime('%d_%m_%y_%H_%M_%S'))
 print(date_string)
 
-with open(date_string+'.csv', 'w', newline='') as csvfile:
+p = os.popen('stress-ng --matrix 0 --tz -t 30m')
+
+with open(date_string + '.csv', 'w', newline='') as csvfile:
     my_writer = csv.writer(csvfile, delimiter=',',
                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
     my_writer.writerow(['Zeit', 'Core 0 Temp', 'Core 1 Temp', 'CPU Freq'])
     i = 0
-    while True:
+    while (i <= STAT_TIME_LIMIT):
         i += 1
 
         time_string = str(datetime.datetime.now().strftime("%X"))
         print(time_string)
 
         core0_temp = subprocess.check_output("sensors | grep 'Core 0'", shell=True, universal_newlines=True)
-        core0_temp = core0_temp[core0_temp.find('+')+1:core0_temp.find('+')+4]
+        core0_temp = core0_temp[core0_temp.find('+') + 1:core0_temp.find('+') + 4]
 
         if core0_temp[2] == '.':
             core0_temp = core0_temp[:2]
@@ -36,7 +40,7 @@ with open(date_string+'.csv', 'w', newline='') as csvfile:
         print(core1_temp)
 
         cpu_frequency = subprocess.check_output("lscpu | grep 'CPU MHz'", shell=True, universal_newlines=True)
-        cpu_frequency = int(cpu_frequency[cpu_frequency.find('.')-4:cpu_frequency.find('.')])
+        cpu_frequency = int(cpu_frequency[cpu_frequency.find('.') - 4:cpu_frequency.find('.')])
         print(cpu_frequency)
 
         my_writer.writerow([time_string, core0_temp, core1_temp, cpu_frequency])
--- install_dependencies_visual_gpu.sh
+++ install_dependencies_visual_gpu.sh
@@ -4,12 +4,12 @@ sudo apt-get install lscpu
 sudo apt-get install htop
 sudo apt-get install stress-ng
 sudo apt-get install lm-sensors
-#sudo apt-get install glmark2
+sudo apt-get install glmark2
 sudo apt-get install indicator-cpufreq
-#sudo apt-get install intel-gpu-tools
+sudo apt-get install intel-gpu-tools
 sudo apt-get install gcc
-#sudo apt-get install qt5-default qt-gtk-platform qt5ct
+sudo apt-get install qt5-default qt-gtk-platform qt5ct
 sudo apt-get install python3-pip
 
-#pip3 install sip
-#pip3 install PyQt5
+pip3 install sip
+pip3 install PyQt5
